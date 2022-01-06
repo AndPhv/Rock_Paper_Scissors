a4a9814f883064441ef6e56f2b5dea9b485ceaed
AndPhv <puhov2001@gmail.com>
2022-01-06 17:03:26 +0300

    Create method Serve and Process

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,8 +1,39 @@
+import com.pengrad.telegrambot.Callback;
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.SendMessage;
+
 public class Bot
 {
-
-    public void serve()
+    private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
+    public void serve() // получения списка обновлений от telegram API (обновления содержат в себе действия со стороны пользователя)
     {
+        bot.setUpdatesListener(updates -> {
+            updates.forEach(this::process); // по штучное обновление метода process
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+        });
+    }
 
+    private void process(Update update)
+    {
+        Message message = update.message();
+        CallbackQuery callbackQuery = update.callbackQuery();
+
+        BaseRequest request = null;
+
+        if (message != null)
+        {
+            long chatId = message.chat().id();
+            request = new SendMessage(chatId, "Hello!");
+        }
+
+        if (request != null)
+        {
+            bot.execute(request);
+        }
     }
 }
