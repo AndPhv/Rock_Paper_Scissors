1ed79b62f8cf2e0e6af1248d628777f5b46722ad
AndPhv <puhov2001@gmail.com>
2022-01-08 19:39:54 +0300

    Create method BuildInlineButton

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,8 +2,12 @@ import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.CallbackQuery;
+import com.pengrad.telegrambot.model.InlineQuery;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.InlineQueryResultArticle;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 
@@ -22,10 +26,16 @@ public class Bot
     {
         Message message = update.message();
         CallbackQuery callbackQuery = update.callbackQuery();
+        InlineQuery inlineQuery = update.inlineQuery();
 
         BaseRequest request = null;
 
-        if (message != null)
+        if (inlineQuery != null)
+        {
+            InlineQueryResultArticle Rock = buildInlineButton("rock", "Rock", "0");
+            InlineQueryResultArticle Paper = buildInlineButton("paper", "Paper", "1");
+            InlineQueryResultArticle Scissors = buildInlineButton("scissors", "Scissors", "2");
+        } else if (message != null)
         {
             long chatId = message.chat().id();
             request = new SendMessage(chatId, "Hello!");
@@ -35,5 +45,15 @@ public class Bot
         {
             bot.execute(request);
         }
+
+    }
+
+    private InlineQueryResultArticle buildInlineButton(String id, String title, String callbackData) {
+        return new InlineQueryResultArticle(id, title, "I'm ready to fight!")
+                .replyMarkup(
+                        new InlineKeyboardMarkup(
+                                new InlineKeyboardButton("Processing...").callbackData(callbackData)
+                        )
+                );
     }
 }
